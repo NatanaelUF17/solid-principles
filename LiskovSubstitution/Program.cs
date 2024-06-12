ec3aba4f4d333adda15315e1b2ac159398b8eed8
nurenaf <nurenaf@evopointsolution.com>
2024-06-11 22:26:57 -0400

    Uploading changes for Liskov Substitution Principle

---
+++ LiskovSubstitution/Program.cs
@@ -0,0 +1,12 @@
+﻿
+static int Area(Rectangle value) => value.Width * value.Height;
+
+var rectangle = new Rectangle(2, 3);
+Rectangle square = new Square();
+
+Console.WriteLine($"{rectangle} has area {Area(rectangle)}");
+
+square.Width = 4;
+square.Height = 4;
+
+Console.WriteLine($"{square} has area {Area(square)}");
---
+++ LiskovSubstitution/Square.cs
@@ -0,0 +1,12 @@
+public class Square : Rectangle
+{
+    public override int Width
+    {
+        set { base.Width = value; }
+    }
+
+    public override int Height
+    {
+        set { base.Height = value; }
+    }
+}
