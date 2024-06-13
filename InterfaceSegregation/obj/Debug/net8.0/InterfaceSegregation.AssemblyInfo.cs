2780590869fe904734efb7a82a395b46124a7acb
nurenaf <nurenaf@evopointsolution.com>
2024-06-12 21:14:29 -0400

    Uploading new stuff

--- InterfaceSegregation/obj/Debug/net8.0/InterfaceSegregation.AssemblyInfo.cs
+++ InterfaceSegregation/obj/Debug/net8.0/InterfaceSegregation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InterfaceSegregation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec3aba4f4d333adda15315e1b2ac159398b8eed8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd5174f8a06359a2a42978f2bc70f84834389590")]
 [assembly: System.Reflection.AssemblyProductAttribute("InterfaceSegregation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InterfaceSegregation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SingleResponsibility/obj/Debug/net8.0/SingleResponsibility.AssemblyInfo.cs
+++ SingleResponsibility/obj/Debug/net8.0/SingleResponsibility.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SingleResponsibility")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a1470bdd78949a8b8312d1be35b841c6db81d6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd5174f8a06359a2a42978f2bc70f84834389590")]
 [assembly: System.Reflection.AssemblyProductAttribute("SingleResponsibility")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SingleResponsibility")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
